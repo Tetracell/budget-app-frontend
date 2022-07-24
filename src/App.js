0d1d3f60baf13ccfd603a73e17d7f6aab1f033fb
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-07-24 16:58:30 -0400

    building skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+
+//Components
+import Home from './Components/Home';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react' //potentially for diaplaying total in navbar
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
