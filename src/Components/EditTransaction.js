a4565030ab46fa6ced70dc7dc5d417f36d5f1ad8
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-07-26 00:28:59 -0400

    Editing readme, other things

--- src/Components/EditTransaction.js
+++ src/Components/EditTransaction.js
@@ -1,26 +1,47 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
-const EditTransaction = ({API}) => {
-    const [transaction, setTransaction] = useState({
-        item_name:"",
-        amount: 0,
-        date:"",
-        from:"",
-        category:"",
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const EditTransaction = ({ API }) => {
+  const [transaction, setTransaction] = useState({
+    item_name: "",
+    amount: 0,
+    date: "",
+    from: "",
+    category: "",
+  });
+
+  let { index } = useParams();
+  useEffect(() => {
+    axios.get(`${API}/transactions/${index}`).then((res) => {
+      setTransaction(res.data);
     });
-
-    let {index} = useParams();
-    useEffect(() => {
-        axios.get(`${API}/transactions/${index}`).then((res) => {
-            setTransaction(res.data);
+  }, []);
+  const putTransaction = () => {
+    axios
+        .put(`${API}/transactions/${index}`, transaction)
+        .then((res) => {
+            setTransaction(res.data)
         })
-    })
+        .then((res) => Navigate(`/transactions`))
+  }
+
+  const handleChange = (event) => {
+    setTransaction({ ...transaction, [event.target.id]: event.target.value });
+  };
+  const handleTransaction = (event) => {
+    event.preventDefault();
+    putTransaction();
+  }
 
   return (
-
-  );
+    <Form>
+        
+    </Form>
+  )
 };
 
 export default EditTransaction;
---
+++ src/Components/ShowTransaction.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ShowTransaction = () => {
+  return (
+    <div>ShowTransaction</div>
+  )
+}
